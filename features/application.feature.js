9b8e112e69409792fe6b1f4fb57b1dfa1ebf3423
boa <boa.matule@gmail.com>
2019-04-17 09:34:11 +0200

    // some line of code

--- features/application.feature.js
+++ features/application.feature.js
@@ -3,7 +3,6 @@ const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
 
 describe('User can input a value and get FizzBuzz results', () => {
-  // Initialize a browser and visit the server's root path
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')
