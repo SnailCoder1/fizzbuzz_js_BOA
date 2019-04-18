13346ce89bbf8c3bd7c3d471b10e64c0057814a7
boa <boa.matule@gmail.com>
2019-04-18 15:11:01 +0200

    finally got all the green! All tests are passing.

--- features/application.feature.js
+++ features/application.feature.js
@@ -2,31 +2,28 @@ const {expect} = require('chai')
 const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
 
+require('../spec.helper');
+
 describe('User can input a value and get FizzBuzz results', () => {
   before(async () => {
-    await browser.init()
-    await browser.visitPage('http://localhost:8080/')
+      await browser.init()
+      await browser.visitPage('http://localhost:8080/');
   });
 
-  // Reload before each test 
   beforeEach(async () => {
-    await browser.page.reload();
-  });
+      await browser.page.reload();
+  })
 
-  // Make sure the browser closes after the test is finished
-  after(() => {
+  after(async ()=> {
     browser.close();
-  });
-
-  // Example test
-    //it('renders the correct page title', async () => {
-     // expect(await browser.page.title()).to.eql('Puppeteer Mocha Scaffold');
-    //});
 
 it('clicking on the "Check" button', async () => {
-    await browser.fillIn("input[id='value']", { with:  "3" })
-    await browser.clickOnButton("input[value='Check']")
-    let content = await browser.getContent("[id='display_answer']")
-    expect(content).to.eql('Fizz');
-  });
-});
+  await browser.fillIn("input[id='value']", { with:  "3" })
+  await browser.clickOnButton("input[value='Check']")
+  let content = await browser.getContent("[id='display_answer']")
+  expect(content).to.eql('Fizz');
+})
+
+})
+})
+
