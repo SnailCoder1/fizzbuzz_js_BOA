3c5631ce80847b2b9dae0c1a772ef669aabe712e
Boa Matule <boa.matule@gmail.com>
2019-06-23 23:00:56 +0200

    Cleans up the files; Styling. WIP.

--- features/application.feature.js
+++ features/application.feature.js
@@ -6,24 +6,23 @@ require('../spec.helper');
 
 describe('User can input a value and get FizzBuzz results', () => {
   before(async () => {
-      await browser.init()
-      await browser.visitPage('http://localhost:8080/');
+    await browser.init()
+    await browser.visitPage('http://localhost:8080/');
   });
 
   beforeEach(async () => {
-      await browser.page.reload();
-  })
+    await browser.page.reload();
+  });
 
   after(async ()=> {
     browser.close();
 
-it('clicking on the "Check" button', async () => {
-  await browser.fillIn("input[id='value']", { with:  "3" })
-  await browser.clickOnButton("input[value='Check']")
-  let content = await browser.getContent("[id='display_answer']")
-  expect(content).to.eql('Fizz');
-})
-
-})
+    it('clicking on the "Check" button', async () => {
+      await browser.fillIn("input[id='value']", { with:  "3" })
+      await browser.clickOnButton("input[value='Check']")
+      let content = await browser.getContent("[id='display_answer']")
+      expect(content).to.eql('Fizz');
+    })
+  })
 })
 
